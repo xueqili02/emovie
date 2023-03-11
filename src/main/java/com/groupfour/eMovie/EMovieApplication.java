bfaae5642536b505c6bbe070b2afedbc50e9f810
xueqili02 <xueqili00@gmail.com>
2023-03-11 20:44:35 +0800

    first commit - project structure

---
+++ src/main/java/com/groupfour/eMovie/EMovieApplication.java
@@ -0,0 +1,13 @@
+package com.groupfour.eMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupfour/eMovie/EMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupfour.eMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
