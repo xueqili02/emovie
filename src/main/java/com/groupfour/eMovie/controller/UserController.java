031c3583b2cc30e79de4be228bd13a6b4f154d42
xueqili02 <xueqili00@gmail.com>
2023-03-18 17:19:24 +0800

    register api;

--- src/main/java/com/groupfour/eMovie/controller/UserController.java
+++ src/main/java/com/groupfour/eMovie/controller/UserController.java
@@ -3,8 +3,13 @@ package com.groupfour.eMovie.controller;
 import com.groupfour.eMovie.entity.User;
 import com.groupfour.eMovie.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -20,21 +25,39 @@ public class UserController {
     }
 
     @PostMapping("")
-    public String registerUser(@RequestBody User user) {
+    public ResponseEntity<Object> registerUser(@RequestBody User user) {
+        User newUser = null;
+        HttpStatus code = null;
+        String message = "";
+        Map<String, Object> map = new HashMap<String, Object>();
+
         try {
-            Assert.notNull(user.getUsername(), "username is null");
-            Assert.notNull(user.getPassword(), "password is null");
-            if (userService.existUser(user.getUsername())) {
-
-            } else {
-                User newUser = userService.registerUser(user.getUsername(), user.getPassword());
+            if (user.getUsername() == null || user.getUsername().equals("")) {
+                code = HttpStatus.BAD_REQUEST;
+                message = "Username is null.";
+                throw new Exception("Username is null.");
+            } else if (user.getPassword() == null || user.getPassword().equals("")) {
+                code = HttpStatus.BAD_REQUEST;
+                message = "Password is null.";
+                throw new Exception("Password is null.");
             }
+
+            if (userService.existUser(user.getUsername())) {
+                code = HttpStatus.BAD_REQUEST;
+                message = "User already exists.";
+                throw new Exception("User already exists.");
+            } else {
+                newUser = userService.registerUser(user.getUsername(), user.getPassword());
+                code = HttpStatus.CREATED;
+                message = "Register Success";
+            }
+
         } catch (Exception e) {
-
+            e.printStackTrace();
         } finally {
-
+            map.put("message", message);
+            map.put("data", newUser);
+            return new ResponseEntity<Object>(map, code);
         }
-
-        return "register";
     }
 }
