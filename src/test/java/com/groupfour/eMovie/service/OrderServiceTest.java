43cab8fddf99a14e5afb06e4f05d2703f055526f
Twicone <2372230014@qq.com>
2023-06-17 17:50:34 +0800

    完成了GenreService、UserService、OrderService和MovieService的单元测试 对相应的Genre、Order、Movie、movieGenre和User等实体进行了equals方法的重载。

--- src/test/java/com/groupfour/eMovie/service/OrderServiceTest.java
+++ src/test/java/com/groupfour/eMovie/service/OrderServiceTest.java
@@ -4,22 +4,46 @@ import com.groupfour.eMovie.dao.OrderDao;
 import com.groupfour.eMovie.entity.Order;
 import com.groupfour.eMovie.service.impl.OrderServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
 public class OrderServiceTest {
-    @Mock
+    @Autowired
     private OrderDao orderDao;
 
     @Mock
     private Order order;
 
-    @InjectMocks
+    @Autowired
     private OrderServiceImpl orderService;
 
     @BeforeEach()
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void callGetAllOrders() {
+        List<Order> orders = orderService.getAllOrders();
+        assertEquals(orders, orderDao.getAllOrders());
+    }
+
+    @Test
+    public void callGetOrdersByUserId() {
+        int[] userIds = {1, 21};
+        for (int id : userIds) {
+            List<Order> orders = orderDao.getOrdersByUserId(id);
+            assertEquals(orders, orderService.getOrdersByUserId(id));
+        }
     }
 }
