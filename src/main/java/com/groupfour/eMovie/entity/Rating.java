db88bc85ad1cf615b5f69cdc986b3f71ceef32fa
Twicone <2372230014@qq.com>
2023-06-17 21:42:20 +0800

    完成了UserService、OrderService中void型方法的单元测试 对Rating和RatingRecord等实体进行了equals方法的重载。

--- src/main/java/com/groupfour/eMovie/entity/Rating.java
+++ src/main/java/com/groupfour/eMovie/entity/Rating.java
@@ -1,5 +1,7 @@
 package com.groupfour.eMovie.entity;
 
+import java.util.Objects;
+
 public class Rating {
     private int uid;
     private int movieid;
@@ -9,6 +11,27 @@ public class Rating {
         this.uid = uid;
         this.movieid = movieid;
         this.rating = rating;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.uid, this.movieid, this.rating);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        Rating rating = (Rating) obj;
+        return rating.getUid() == this.uid
+                && rating.getMovieid() == this.movieid
+                && rating.getRating() == this.rating;
     }
 
     public int getUid() {
--- src/test/java/com/groupfour/eMovie/service/UserServiceTest.java
+++ src/test/java/com/groupfour/eMovie/service/UserServiceTest.java
@@ -1,16 +1,26 @@
 package com.groupfour.eMovie.service;
 
+import com.groupfour.eMovie.dao.MovieDao;
 import com.groupfour.eMovie.dao.UserDao;
+import com.groupfour.eMovie.entity.Movie;
+import com.groupfour.eMovie.entity.Rating;
+import com.groupfour.eMovie.entity.RatingRecord;
 import com.groupfour.eMovie.entity.User;
 import com.groupfour.eMovie.service.impl.UserServiceImpl;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
@@ -24,6 +34,9 @@ public class UserServiceTest {
 
     @Autowired
     private UserServiceImpl userService;
+
+    @Autowired
+    private MovieDao movieDao;
 
     @BeforeEach()
     public void setUp() {
@@ -70,27 +83,63 @@ public class UserServiceTest {
         }
     }
 
-//    @Test
-//    public void callUpdateUser() {
-//        UserService userService = Mockito.mock(UserService.class);
-//        Mockito.doNothing().when(userService).updateUser(Mockito.isA(User.class));
-//        userService.updateUser(user);
-//        Mockito.verify(userService, Mockito.times(1)).updateUser(user);
-//    }
-//
-//    @Test
-//    public void callChangePassword() {
-//
-//    }
-//
-//    @Test
-//    public void getRatingRecord() {
-//        int[] uids = {1, 2};
-//        for (int i : uids) {
-//            List<Rating> ratingList = userDao.getRatings(i);
-//            List<RatingRecord> recordList = userService.getRatingRecord(i);
-//            assertEquals(recordList, );
-//        }
-//
-//    }
+    @Test
+    public void callUpdateUser() {
+        userService.updateUser(user);
+        UserServiceImpl userService = Mockito.mock(UserServiceImpl.class);
+        Mockito.doAnswer(invocation -> {
+            Object arg = invocation.getArgument(0);
+            assertEquals(user, arg);
+            return null;
+        }).when(userService).updateUser(Mockito.any(User.class));
+        //Mockito.verify(userService, Mockito.times(1)).updateUser(user);
+        this.userService.updateUser(user);
+    }
+
+    @Test
+    public void callChangePassword() {
+        String username = "lyx";
+        String password = "123456";
+        UserServiceImpl userService = Mockito.mock(UserServiceImpl.class);
+        Mockito.doAnswer(invocation -> {
+            Object arg0 = invocation.getArgument(0);
+            Object arg1 = invocation.getArgument(1);
+            assertEquals(username, arg1);
+            assertEquals(password, arg0);
+            return null;
+        }).when(userService).changePassword(Mockito.any(String.class), Mockito.any(String.class));
+
+        this.userService.changePassword(password, username);
+    }
+
+    @Test
+    public void callRateMovie() {
+        int uid = 2;
+        int movieId = 168;
+        float rating = 3;
+        Rating r = new Rating(uid, movieId, rating);
+        UserServiceImpl userService = Mockito.mock(UserServiceImpl.class);
+        Mockito.doAnswer(invocation -> {
+            Object arg0 = invocation.getArgument(0);
+            assertEquals(r, arg0);
+            return null;
+        }).when(userService).rateMovie(Mockito.any(Rating.class));
+
+        this.userService.rateMovie(r);
+    }
+
+    @Test
+    public void getRatingRecord() {
+        int[] uids = {1, 2};
+        for (int i : uids) {
+            List<Rating> ratingList = userDao.getRatings(i);
+            List<RatingRecord> recordList = new ArrayList<>();
+            for (Rating r : ratingList) {
+                Movie movie = movieDao.getMovieByUnusedId(r.getMovieid());
+                recordList.add(new RatingRecord(movie.getOriginalTitle(), r.getRating()));
+            }
+            assertEquals(recordList, userService.getRatingRecord(i));
+        }
+
+    }
 }
